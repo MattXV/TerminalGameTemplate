728321571a2eef6772f0ba89b6c76ca6854b6c12
Mattia <mattia.clmb@gmail.com>
2024-10-01 17:48:56 +0100

    Added template stuff.

--- 3DTerm.cpp
+++ 3DTerm.cpp
@@ -1,65 +1,15 @@
 #include <curses.h>
-#include <windows.h>
-#include <chrono>
 #include <string>
 #include <cstdio>
 #include <functional>
+#include "Utility.h"
 #include <thread>
 
 
-using namespace std::chrono;
-
-
-
-class Time {
-public:
-    static void Init() { lastFrameTime = steady_clock::now(); }
-    static void Update() {
-        const steady_clock::time_point now = steady_clock::now();
-        const duration<double> dt_ms = now - lastFrameTime;
-        deltaTime = dt_ms.count();
-        
-       
-    }
-    static void SetLastFrameTime() { lastFrameTime = steady_clock::now(); }
-
-    static double getDeltaTime() { return deltaTime; }
-private:
-    static steady_clock::time_point lastFrameTime;
-    static double deltaTime;
-};
-double Time::deltaTime = 0;
-steady_clock::time_point Time::lastFrameTime = steady_clock::now();
-
-
-template<typename... FormatParameters>
-std::string formatString(const char* format, const FormatParameters&... pars) {
-    int stringSize = snprintf(nullptr, 0, format, pars...) + 1;
-    if (stringSize < 0) return std::string();
-    
-    const int stackThresh = 64;
-    char stackAll[stackThresh];
-    char* str = nullptr;
-    if (stringSize < stackThresh) {
-        str = &stackAll[0];
-    }
-    else {
-        str = new char[stringSize];
-
-    }
-
-    snprintf(str, stringSize, format, pars...);
-    std::string outString((const char*)str);
-    if (!(stringSize < stackThresh))
-        delete[] str;
-    
-    
-    return outString;
-}
-
-
-
 int main() {
+    // 
+    // Setup Phase
+    // 
 
 
     // Initialize ncurses
@@ -70,44 +20,50 @@ int main() {
     keypad(stdscr, TRUE);
     nodelay(stdscr, TRUE);
 
-    // Set the refresh rate
+    // Rendering options
     int frameRate = 60;
     int delay = 1000 / frameRate;
 
-    int x = 10;
-    int y = 10;
 
     int screenHeight, screenWidth;
         getmaxyx(stdscr, screenHeight, screenWidth);
+    auto lastFrameTime = high_resolution_clock::now();
+
+    // Add colours here :)
+    start_color();
+    init_pair(1, COLOR_WHITE, COLOR_BLACK);
+    init_pair(2, COLOR_BLACK, COLOR_RED);
+    init_pair(3, COLOR_BLACK, COLOR_GREEN);
+
+
+    // Game 'Assets' and game data lol
     int ballX = 0;
     int ballY = 20;
 
     int ballDirection = 1;
 
 
-
-    auto lastFrameTime = high_resolution_clock::now();
-
-
     double eventTimer = 0;
     double jumpTimer = 0;
     bool jumping = false;
-    double timer = 0;
 
     // Main game loop
     Time::Init();
     while (true) {
-        // start with a blank canvas
+        // 
+        // Update Phase
+        // 
+
 
         // Calculate deltaTime
         Time::Update();
-        double deltaTime = Time::getDeltaTime();
        
+
 
         // Handle user input and Update game state
         int userInput = getch();
         if (userInput == 'q') {
-            break; // Exit the game if 'q' is pressed
+            break; // Exit the loop and terminate the game if 'q' is pressed
         }
         else if (userInput == KEY_RIGHT) {
             ballDirection = 1; // Move right if the right arrow key is pressed
@@ -121,10 +77,12 @@ int main() {
             }
         }
 
-        // More rendering...
-        eventTimer += deltaTime;
+        if (ballX > screenWidth || ballX < 0) {
+            ballDirection *= -1; // Reverse direction on collision with edges
+        }
+
+        eventTimer += Time::getDeltaTime();
         if (eventTimer > .02) {
-            //ballDirection *= -1;
             ballX += ballDirection;
             eventTimer = 0;
 
@@ -134,7 +92,6 @@ int main() {
             else {
                 ballY = 20;
             }
-
         }
 
         //ballX += ballDirection;
@@ -147,30 +104,38 @@ int main() {
         }
 
 
-        if (ballX > screenWidth || ballX < 0) {
-            ballDirection *= -1; // Reverse direction on collision with edges
-        }
-
+        // 
+        // Rendering Phase
+        // 
+        
+        // Clear the screen first
         erase();
         // Render game objects using mvprintw
         //mvprintw(y, x, "Character or object symbol");
-        timer += deltaTime;
-        //std::string timeStr = std::string("DeltaTime: ") + std::to_string(deltaTime) + std::string("Timer: ") + std::to_string(timer);
-        
-        mvprintw(10, 10, formatString("%s DeltaTime: %f", "Loooooooooooooooooooooooooooong", Time::getDeltaTime()).c_str());
 
-        
 
+        // Standard colour
+        attron(COLOR_PAIR(1));
         // Draw the ball on the screen
         mvprintw(ballY, ballX, "O");
 
-        // Refresh the screen
+        // UI? Can you call it that?
+        mvprintw(0, 0, formatString("%s | DeltaTime: %f", "Henlo frens", Time::getDeltaTime()).c_str());
+        // Change colour - see top of the function for colour setup
+        attron(COLOR_PAIR(2));
+        mvprintw(5, 5, "This should be printed in black with a red background!\n");
+
+        attron(COLOR_PAIR(3));
+        mvprintw(7, 5, "And this in a green background!\n");
+        
+
+        // Update the terminal
         refresh();
-        //Sleep(delay);
-        //usleep(delay);
+
 
         Time::SetLastFrameTime();
-        std::this_thread::sleep_for(milliseconds(12));
+        std::this_thread::sleep_for(milliseconds(delay));
+
     }
 
     // Cleanup ncurses
